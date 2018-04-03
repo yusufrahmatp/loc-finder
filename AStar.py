0820fb101ce181fe91f89ddf877b6da9c4dbe269
annisasekaar <36126881+annisasekaar@users.noreply.github.com>
2018-04-04 02:11:06 +0700

    Functions to get shortest path using A* algorithm

---
+++ AStar.py
@@ -0,0 +1,58 @@
+def AStar(_adjacencyMatrix, _distanceMatrix, _start, _end) :
+# mencari jalan terdekat dari dua buah titik menggunakan fungsi A*
+# mengembalikan list of path
+	listOfPath = []
+	closestInAdj = []
+	next = _start		# ini adalah nama simpul
+	distanceSaved = 0	# ini adalah jarak yang telah ditempuh
+	closestInAdj.append(_start)			# inisialisasi
+	closestInAdj.append(distanceSaved)		# inisialisasi
+	
+	while (next != _end) :
+		closestInAdj = subAStar(_adjacencyMatrix, _distanceMatrix, next, _end, distanceSaved)
+		next = closestInAdj[0]
+		distanceSaved = closestInAdj[1]		
+		listOfPath.append(next)	
+	return listOfPath
+	
+
+def subAStar(_adjacencyMatrix, _distanceMatrix, current, _end, distanceSaved) :
+# mencari f(n) dengan nilai paling kecil dari seluruh simpul yang bertetangga dengannya
+# mengembalikan titik berupa integer
+	closest = []
+	closest.append(current) 															# inisialisasi simpul dengan jarak terdekat. Berupa matriks
+	closest.append(getLongestDistance(current, _end, _distanceMatrix, distanceSaved))	# indeks pertama (0) adalah nama simpulnya dan indeks kedua (1) adalah jarak terjauh
+	
+	for i in range(len(_adjacencyMatrix[current])) :
+		if (_adjacencyMatrix[current][i] != 0) :
+			#print(getFn(current, i, distanceSaved, _distanceMatrix))
+			if (getFn(current, i, _end, distanceSaved, _distanceMatrix) < closest[1]) :
+				closest[0] = i
+				closest[1] = getGn(current, i, distanceSaved, _distanceMatrix)
+			elif (getFn(current, i, _end, distanceSaved, _distanceMatrix) == closest[1]) :
+				if (i == _end) :
+					closest[0] = i
+					closest[1] = getGn(current, i, distanceSaved, _distanceMatrix)
+	return closest
+
+
+def getLongestDistance (current, _end, _distanceMatrix, distanceSaved) :
+# mencari f(n) dengan nilai terbesar, yaitu gabungan dari g(n) terbesar dan h(n) terbesar
+	longestDistance = 0
+	longestHn = 0
+	for i in range(len(_distanceMatrix[current])) :
+		if (_distanceMatrix[current][i] > longestDistance) :	# mencari jarak antar simpul dengan nilai terbesar
+			longestDistance = _distanceMatrix[current][i]
+		if (getHn(i, _end, _distanceMatrix) > longestHn) :
+			longestHn = getHn(i, _end, _distanceMatrix)			# mencari h(n) dengan nilai terbesar
+	return (longestDistance + distanceSaved + longestHn)
+
+def getFn(current, next, _end, distanceSaved, _distanceMatrix) :
+	return getGn(current, next, distanceSaved, _distanceMatrix) + getHn(next, _end, _distanceMatrix)
+
+def getGn(current, next, distanceSaved, _distanceMatrix)	:
+	return (_distanceMatrix[current][next] + distanceSaved)
+	
+def getHn(current, next, _distanceMatrix) :
+# h(n) adalah jarak heuristik. dapat diambil dengan cara melihat ke _distanceMatrix
+	return _distanceMatrix[current][next]
